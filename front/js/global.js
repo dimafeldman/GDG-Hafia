b54c45e47fd22720d6ec4cc7e19f27b08ea0603c
Dima <dima.feldman@fiverr.com>
2013-11-21 16:41:19 +0200

    js cleanup

--- front/js/global.js
+++ front/js/global.js
@@ -32,67 +32,8 @@ Gdg.controller("personsController", function ($scope, personsList) {
         orderBy : 'name'
     };
 
-    // METHOD: handles all activites ajax calls
-    var personsAjax = function(actionType, id, name, val, successCallback, errorCallback) {
-
-        actionType      = (actionType != null)                   ? actionType      : 'update';
-        successCallback = (typeof successCallback == 'function') ? successCallback : function(){};
-        errorCallback   = (typeof errorCallback == 'function')   ? errorCallback   : function(){};
-
-        // set default ajax setting
-        var ajaxSettings = {
-            type       : 'post',
-            url        : '/',
-            dataType   : 'json',
-            data       : {},
-            error      : function(response) {
-                console.log('something went wrong');
-                errorCallback(response);
-            },
-            statusCode : {
-                200: function(response) {
-
-                    if (response.errors) {
-                        console.log(response.errors);
-                        errorCallback(response.errors);
-                    } else {
-                        console.log('all good');
-                        successCallback();
-                    }
-                }
-            }
-        };
-
-        // set ajax setting according to the actionType
-        switch(actionType) {
-            case 'add':
-                ajaxSettings.type = 'post';
-                ajaxSettings.url  = '/persons/add';
-                ajaxSettings.data = {
-                    name        : name,
-                    value       : val
-                };
-            break;
-            case 'update':
-                ajaxSettings.type = 'put';
-                ajaxSettings.url  = '/persons/update';
-                ajaxSettings.data = {
-                    person_id : id,
-                    name        : name,
-                    value       : val
-                };
-            break;
-            case 'delete':
-                ajaxSettings.type = 'delete';
-                ajaxSettings.url  = '/persons/delete';
-                ajaxSettings.data = {
-                    preson_id : id
-                };
-            break;
-        }
-
-        $.ajax(ajaxSettings);
-    };
+    // Fetch data
+    //$scope.presons = $resource('/persons.json');
 
     return $scope.personsController = this;
 });
